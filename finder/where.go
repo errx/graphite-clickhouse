1ffd0966b5efd1438a4f4c2ce48d8705ee515698
Oleg Matrokhin <oyumatrokhin@avito.ru>
2018-03-20 15:00:24 +0300

    fix tilda; skip test

--- finder/where.go
+++ finder/where.go
@@ -11,13 +11,12 @@ import (
 func GlobToRegexp(g string) string {
 	s := g
 	s = strings.Replace(s, ".", "[.]", -1)
-	s = strings.Replace(s, "*", "([^.]*?)", -1)
-	s = strings.Replace(s, "~", "(.*?)", -1)
 	s = strings.Replace(s, "{", "(", -1)
 	s = strings.Replace(s, "}", ")", -1)
 	s = strings.Replace(s, "?", "[^.]", -1)
 	s = strings.Replace(s, ",", "|", -1)
 	s = strings.Replace(s, "*", "([^.]*?)", -1)
+	s = strings.Replace(s, "~", "(.*?)", -1)
 	return s
 }
 
